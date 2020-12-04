65e3afbc8804acff3d2497a58ad9ece1005f0311
mittalgarima <garimamittal101@gmail.com>
2020-12-04 20:30:48 +0530

    new commit

---
+++ src/components/Results.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+const Results = (props) => {
+    const { repos } = props;
+    console.log("Repos is: ",repos.data);
+
+    const listRepos = repos.length!==0 ?(
+        repos.data.slice(0, 5).map((item) => <li key={item.id}><a href={item.html_url}>{item.name}</a></li>)
+    ):(
+    <li>NO repos found</li>)
+    ;
+   return <ul>
+  {listRepos}
+   </ul>;
+};
+export default Results;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React,{ useState } from "react";
+import axios from 'axios'
+import Results from './Results';
+
+const SearchBar = () => {
+   const [searchInput,setSearchInput] = useState(" ");
+   const [repos,setRepos]= useState([]);
+
+
+   const handleChange = (e)=>{
+      setSearchInput(e.target.value)
+
+   }
+
+   const handleClick= async ()=>{
+      console.log(searchInput);
+ 
+     try{
+
+  const result = await axios(`https://api.github.com/users/${searchInput}/repos`);
+
+  setRepos(result);}
+  catch (err){
+   console.log(err);
+} };
+
+
+   return (<>
+   <div style={{padding:'20px'}}>
+      <input type="text" placeholder="search" value={searchInput} onChange={handleChange}/>
+      <button onClick={handleClick}>Search</button>
+   </div>
+   <Results repos={repos} />
+   </>
+   );
+};
+export default SearchBar;
